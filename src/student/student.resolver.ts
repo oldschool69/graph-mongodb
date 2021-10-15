4e5dc411fa13fb559e36c5edd201e0c7318e142b
Flavio Oliveira <flaviomo@motorola.com>
2021-10-15 16:56:26 -0300

    complete chanllenge 2

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
 import { StudentService } from './student.service';
@@ -6,6 +6,16 @@ import { StudentService } from './student.service';
 @Resolver(() => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(() => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
+
+  @Query(() => StudentType)
+  async student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
 
   @Mutation((returns) => StudentType)
   async createStudent(
